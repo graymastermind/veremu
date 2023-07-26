1d4c104803a31d40734b42260e62821ea26d06be
taurai <taurai.developer@gmail.com>
2023-07-26 17:34:37 +0200

    with last input

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import datetime
 from flask import Flask, request, g
 from twilio.twiml.messaging_response import MessagingResponse
 
-
 app = Flask(__name__)
 
 
@@ -16,6 +15,7 @@ def get_db():
     if db is None:
         db = g._database = sqlite3.connect("chatbot.db")
     return db
+
 
 # Create tables for notifications, users, and assignments (if they don't exist)
 def create_tables():
@@ -47,8 +47,10 @@ def create_tables():
 
         get_db().commit()
 
+
 # Initialize the database
 create_tables()
+
 
 def generate_user_id():
     # Generate a user ID prefixed with the current year and a random 3-digit number
@@ -58,6 +60,7 @@ def generate_user_id():
     user_id = current_year + random_number + random_letter
     return user_id
 
+
 # Close the database connection when the app is finished
 @app.teardown_appcontext
 def close_connection(exception):
@@ -65,6 +68,8 @@ def close_connection(exception):
     if db is not None:
         db.close()
 
+
+lastInput = ''
 @app.route("/sms", methods=["POST"])
 def reply_to_sms():
     """Respond to incoming SMS with the same message."""
@@ -74,25 +79,27 @@ def reply_to_sms():
     twilio_response = MessagingResponse()
     twilio_response.message(incoming_message)
 
+    global lastInput
+    lastInput = incoming_message
 
-    if incoming_message.lower() == 'hi':
+    if incoming_message.lower() == 'hi' & lastInput == '':
         twilio_response.message(
             "Welcome! Ruwa Vocational Training Centre:\n0. Register\n1. Ask a question\n2. View Notifications\n3. Update "
-            "Profile\n4. Submit Assignment\n5. Exit")
-    elif incoming_message == '0':
+            "Profile\n4. Submit Assignment\n5. Assignment Results\n6. Financial Account\n7. Examination Dates\n8. Exit")
+    elif lastInput == '' & incoming_message == '0':
         # Handle "Register" option
         twilio_response.message("You chose option 0 - Register")
         # Ask the user for their username
         twilio_response.message("Please enter your username:")
-    elif incoming_message == '1':
+    elif lastInput == '' & incoming_message == '1':
         # Handle "Ask a question" option
         twilio_response.message("You chose option 1 - Ask a question")
         # Implement the logic to handle asking a question here
-    elif incoming_message == '2':
+    elif lastInput == '' & incoming_message == '2':
         # Handle "View Notifications" option
         twilio_response.message("You chose option 2 - View Notifications")
         # Implement the logic to handle viewing notifications here
-    elif incoming_message == '3':
+    elif lastInput == '' & incoming_message == '3':
         # Handle "Update Profile" option
         twilio_response.message("You chose option 3 - Update Profile")
         # Check if the user is registered
@@ -104,20 +111,20 @@ def reply_to_sms():
             twilio_response.message("Please enter your new username:")
         else:
             twilio_response.message("You are not registered. Please register first.")
-    elif incoming_message == '4':
+    elif lastInput == '' & incoming_message == '4':
         # Handle "Submit Assignment" option
         twilio_response.message("You chose option 4 - Submit Assignment")
         # Implement the logic to handle submitting an assignment here
-    elif incoming_message == '5':
+    elif lastInput == '' & incoming_message == '5':
         # Handle "Assignment Results" option
         twilio_response.message("You chose option 5 - Assignment Results")
-    elif incoming_message == '6':
+    elif lastInput == '' & incoming_message == '6':
         # Handle "Financial Account" option
         twilio_response.message("You chose option 6 - Financial Account")
-    elif incoming_message == '7':
+    elif lastInput == '' & incoming_message == '7':
         # Handle "Examination Dates" option
         twilio_response.message("You chose option 7 - Examination Dates")
-    elif incoming_message == '8':
+    elif lastInput == '' & incoming_message == '8':
         # Handle "Exit" option
         twilio_response.message("Goodbye!")
     else:
@@ -130,7 +137,8 @@ def reply_to_sms():
             # Store the username and user ID in the database
             username = incoming_message
             user_id = generate_user_id()
-            cursor.execute("UPDATE users SET username=?, user_id=? WHERE phone_number=?", (username, user_id, request.form['From']))
+            cursor.execute("UPDATE users SET username=?, user_id=? WHERE phone_number=?",
+                           (username, user_id, request.form['From']))
             get_db().commit()
             twilio_response.message(f"Welcome, {username}! Your user ID is {user_id}.")
         elif existing_user and existing_user[1]:
@@ -144,6 +152,7 @@ def reply_to_sms():
             twilio_response.message("Invalid choice. Please try again.")
 
     return str(twilio_response)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
